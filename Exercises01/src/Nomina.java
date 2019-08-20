d300274bd7e77adbcf418d16174f94b1ec7c394a
maherow <51948738+maherow@users.noreply.github.com>
2019-08-20 18:48:19 +0200

    Update Nomina.java

--- Exercises01/src/Nomina.java
+++ Exercises01/src/Nomina.java
@@ -1,5 +1,23 @@
-package clase_empleado;
+package clases;
+
+import java.util.Date;
+
 public class Nomina {
 
-
+	private Date fecha_alta;
+    private Date fecha_baja;
+    private long sueldo_base;
+    private long complemento;
+    private final float porcentaje=0.10f;
+    
+	public float calculaNomina (Date fechaAlta, Date fechaBaja, String categoria) {
+		
+		if (fechaBaja.compareTo(fechaAlta)>5) {complemento=1000;}
+		
+		
+		long nomina=10_000;
+		
+		return nomina;
+	}
+    
 }
